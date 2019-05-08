2d15774dfbf507047b12e7c6220086b6e3985e9f
Daniela Mead <Daniela Mead>
2019-05-08 17:20:13 -0400

    adds HOC to root app.js

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -7,32 +7,38 @@ import firebase from '../firebase'
 /**
  * COMPONENT
  */
-const AuthForm = props => {
-  const { name, displayName, handleSubmit, error } = props
+class AuthForm extends React.Component {
+  render() {
+    const { name, displayName, handleSubmit, error } = this.props
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      {/* <a href="/auth/google">{displayName} with Google</a> */}
-    </div>
-  )
+    return (
+      <div>
+        <form onSubmit={handleSubmit} name={name}>
+          <div>
+            <label htmlFor="email">
+              <small>Email</small>
+            </label>
+            <input name="email" type="text" />
+          </div>
+          <div>
+            <label htmlFor="password">
+              <small>Password</small>
+            </label>
+            <input name="password" type="password" />
+          </div>
+          <div>
+            <button type="submit">{displayName}</button>
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
+        </form>
+        {
+          user
+            ? <button type="button" onClick={signOut}>Sign Out</button>
+            : <button type="button" onClick={signInWithGoogle}>Sign in with Google</button>
+        }
+      </div>
+    )
+  }
 }
 
 /**
