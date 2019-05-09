64b38d9e54298903646861ffc30321d32b15af45
Kim Ma <kimma1261@gmail.com>
2019-05-09 16:48:19 -0400

    fixes add travler bug

--- client/components/AddTravelers.js
+++ client/components/AddTravelers.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import firebase from '../firebase'
 
-
 class AddTravelers extends React.Component {
   constructor() {
     super()
     this.state = {
       emails: {
-          email1: ''
+        email1: ''
       },
       numEmails: 1
     }
@@ -22,45 +21,47 @@ class AddTravelers extends React.Component {
   }
 
   updateEmail = event => {
-      console.log('event.target is', event.target)
-      console.log('event.target.name is', event.target.name)
-      event.persist()
-      this.setState((prevState) => {
-        console.log('event is', event)
-          console.log('event.target is now', event.target)
-          return {emails: {
-            ...prevState.emails, [event.target.name]: event.target.value
+    console.log('event.target is', event.target)
+    console.log('event.target.name is', event.target.name)
+    event.persist()
+    this.setState(prevState => {
+      console.log('event is', event)
+      console.log('event.target is now', event.target)
+      return {
+        emails: {
+          ...prevState.emails,
+          [event.target.name]: event.target.value
+        }
       }
-    }})
-    }
+    })
+  }
 
   addEmail = async () => {
-      // when this number increments, the state will change
-      // which will trigger... what?
-      // we want it to trigger the render of a new line on the form
-      //I guess the emails could be an array
-      //or an object
-      //and then we can iterate over it to create the component
+    // when this number increments, the state will change
+    // which will trigger... what?
+    // we want it to trigger the render of a new line on the form
+    //I guess the emails could be an array
+    //or an object
+    //and then we can iterate over it to create the component
 
     await this.setState((prevState, props) => {
-            let newNum = prevState.numEmails + 1
-            return {numEmails: newNum, 
-            emails: {
-                ...prevState.emails,
-                [`email${newNum}`]: ''
-            }}
+      let newNum = prevState.numEmails + 1
+      return {
+        numEmails: newNum,
+        emails: {
+          ...prevState.emails,
+          [`email${newNum}`]: ''
+        }
+      }
     })
 
     console.log('num emails is now', this.state.numEmails)
     console.log(this.state)
 
     //add a new email key to the state
-
-
-
   }
 
-  addFriend = async event => {
+  addFriend = event => {
     event.preventDefault()
 
     //how can I make this form send someone an email on submit?
@@ -69,6 +70,7 @@ class AddTravelers extends React.Component {
     this.setState({
       email1: ''
     })
+    this.props.history.push('/preference')
   }
 
   render() {
@@ -76,23 +78,26 @@ class AddTravelers extends React.Component {
       <div>
         <h3>Add Your Friends!</h3>
         <form onSubmit={this.addFriend}>
-            {Object.keys(this.state.emails).map(key => {
-                return (<div key={key}>
+          {Object.keys(this.state.emails).map(key => {
+            return (
+              <div key={key}>
                 <input
-                name={key}
-                type="text"
-                onChange={this.updateEmail}
-                value={this.state[key]}
+                  name={key}
+                  type="text"
+                  onChange={this.updateEmail}
+                  value={this.state.emails[key]}
                 />
-                </div>)
-            }
-            )}
-            <div>
-                <button type="button" id="addEmail" onClick={this.addEmail}>+</button>
-            </div>
-            <div>
+              </div>
+            )
+          })}
+          <div>
+            <button type="button" id="addEmail" onClick={this.addEmail}>
+              +
+            </button>
+          </div>
+          <div>
             <button type="submit">Send Invites</button>
-            </div>
+          </div>
         </form>
       </div>
     )
