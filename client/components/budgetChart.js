a176494de6a1b89d6698e28a76ee0a7088048703
Daniela Mead <Daniela Mead>
2019-05-10 15:54:06 -0400

    pie chart

--- client/components/budgetChart.js
+++ client/components/budgetChart.js
@@ -1,28 +1,6 @@
 import React, { PureComponent } from 'react'
 import { PieChart, Pie, Sector, Cell } from 'recharts'
 
-const data = [
-  { name: '<150', value: 0 },
-  { name: '150-500', value: 0 },
-  { name: '500-1000', value: 0 },
-  { name: '1000-1500', value: 0 },
-  { name: '>1500', value: 0 }
-];
-
-prefs.map(prefObj => prefObj.budget).forEach(budgetRange => {
-  if (budgetRange === data[0].name) {
-    data[0].value++
-  } else if (budgetRange === data[1].name) {
-    data[1].value++
-  } else if (budgetRange === data[2].name) {
-    data[2].value++
-  } else if (budgetRange === data[3].name) {
-    data[3].value++
-  } else {
-    data[4].value++
-  }
-})
-
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#00B22E'];
 
 const RADIAN = Math.PI / 180;
@@ -41,13 +19,46 @@ const renderCustomizedLabel = ({
 };
 
 export default class BudgetChart extends PureComponent {
-
+  constructor() {
+    super()
+    this.state = {
+      budgetPrefs: [
+        { name: '< $150', value: 0 },
+        { name: '$150-$500', value: 0 },
+        { name: '$500 - $1000', value: 0 },
+        { name: '$1000-$1500', value: 0 },
+        { name: '$1500+', value: 0 }
+      ]
+    }
+  }
+  componentDidMount() {
+    setTimeout(async () => {
+      const data = [...this.state.budgetPrefs]
+      console.log("array of preferences", this.props.arrayPrefs)
+      this.props.arrayPrefs.map(prefObj => prefObj.budget).forEach(budgetRange => {
+        if (budgetRange === data[0].name) {
+          data[0].value += 1
+        } else if (budgetRange === data[1].name) {
+          data[1].value += 1
+        } else if (budgetRange === data[2].name) {
+          data[2].value += 1
+        } else if (budgetRange === data[3].name) {
+          data[3].value += 1
+        } else {
+          data[4].value += 1
+        }
+      })
+      await this.setState({
+        budgetPrefs: [...data]
+      })
+    })
+  }
   render() {
     return (
       <div>
         <PieChart width={600} height={600}>
           <Pie
-            data={data}
+            data={this.state.budgetPrefs}
             cx={200}
             cy={200}
             labelLine={false}
@@ -57,7 +68,7 @@ export default class BudgetChart extends PureComponent {
             dataKey="value"
           >
             {
-              data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+              this.state.budgetPrefs.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
             }
           </Pie>
         </PieChart>
--- client/components/visual.js
+++ client/components/visual.js
@@ -2,35 +2,53 @@
 
 import firebase from '../firebase'
 import React from 'react'
-// import BudgetChart from './BudgetChart'
+import BudgetChart from './BudgetChart'
 
 class Visual extends React.Component {
-  componentDidMount() {
-    let arrayPrefs = []
-    const firebaseDB = firebase.firestore()
-    firebaseDB
-      .collection('preferences')
-      .where('trip', '==', this.props.match.params.tripId)
-      .get()
-      .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-          arrayPrefs.push(doc.data())
-        })
-      })
-      .catch(function(error) {
-        console.log('Error getting documents: ', error)
-      })
-    console.log(arrayPrefs)
+  constructor() {
+    super()
+    this.state = {
+      arrayPrefs: []
+    }
   }
+  getData() {
+    setTimeout(async () => {
+      const data = []
+      const firebaseDB = await firebase.firestore()
+      await firebaseDB
+        .collection('preferences')
+        .where('trip', '==', this.props.match.params.tripId)
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            data.push(doc.data())
+          })
+        })
+        .catch(function (error) {
+          console.log('Error getting documents: ', error)
+        })
+      await this.setState({
+        arrayPrefs: [...data]
+      })
+    })
 
+  }
+  componentDidMount() {
+    this.getData()
+  }
   render() {
-    console.log(this.arrayPrefs)
-    return (
-      <div>
-        <h1>Trip Preferences:</h1>
-        {/* <BudgetChart arrayPrefs={this.arrayPrefs} /> */}
-      </div>
-    )
+    if (this.state.arrayPrefs.length < 1) {
+      return (
+        <div>Loading...</div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>Trip Preferences:</h1>
+          <BudgetChart arrayPrefs={this.state.arrayPrefs} />
+        </div>
+      )
+    }
   }
 }
 
