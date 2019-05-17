60d9459d51ef2030b1cf5c106e9d971382025190
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-05-17 12:30:30 -0400

    removes message and replaces with just button

--- client/components/MainPage.js
+++ client/components/MainPage.js
@@ -3,20 +3,20 @@ import React from 'react'
 // import signup from './signup'
 
 class MainPage extends React.Component {
-
-render () {
+  render() {
     return (
-        <div id="mainPage">
+      <div id="mainPage">
         <div id="mainTitle">Flock.</div>
-        <div id="mainDescription">Flock is a revolutionary new way to collaborate with your friends on planning travel.</div>
-        <div id="lowerDescription">Come Fly With Us!</div>
-        <button type="button">Come Fly With Us!</button>
+        <div id="mainDescription">
+          Flock is a revolutionary new way to collaborate with your friends on
+          planning travel.
         </div>
+        <div id="lowerDescription">
+          <button type="button">Come Fly With Us!</button>
+        </div>
+      </div>
     )
-
-}
-
-
+  }
 }
 
 export default MainPage
