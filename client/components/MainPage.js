aed9fa566ecca4c13c23b36462dea757e6b6d860
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-05-18 22:19:01 -0400

    fixes splash page button and centers it

--- client/components/MainPage.js
+++ client/components/MainPage.js
@@ -6,41 +6,39 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 // import signup from './signup'
 
 class MainPage extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-          redirect: false
-        }
-      }
+  constructor() {
+    super()
+    this.state = {
+      redirect: false
+    }
+  }
 
-      setRedirect = () => {
-        this.setState({
-          redirect: true
-        })
-      }
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
 
   render() {
     if (this.state.redirect) {
-        return <Redirect to='/signup' />
-    }
-
-    else {
-    return (
-      <div id="mainPage">
-        <div id="mainTitle">Flock.</div>
-        <div id="mainDescription">
-          Flock is a revolutionary new way to collaborate with your friends on
-          planning travel.
-        </div>
-        <ButtonToolbar >
-          <div id="lowerDescription">
-            <Button variant="outline-light"
-            onClick={this.setRedirect}
-            >Come Fly With Us!</Button>
+      return <Redirect to="/new" />
+    } else {
+      return (
+        <div id="mainPage">
+          <div id="mainTitle">Flock.</div>
+          <div id="mainDescription">
+            Flock is a revolutionary new way to collaborate with your friends on
+            planning travel.
           </div>
-        </ButtonToolbar>
-      </div>
-    )
+          <div id="lowerDescription">
+            <ButtonToolbar>
+              <Button variant="outline-light" onClick={this.setRedirect}>
+                Come Fly With Us!
+              </Button>
+            </ButtonToolbar>
+          </div>
+        </div>
+      )
     }
   }
 }
